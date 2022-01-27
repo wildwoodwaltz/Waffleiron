d99a7dc327ce8778919d0d35f45126ff00246b43
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-01-27 12:06:23 -0800

    Fixed CSS linking and Added Function

--- js/active.js
+++ js/active.js
@@ -1,31 +1,41 @@
 
+
 alert("Before we begin, there are a few questions we would like to ask.");
 
-let wyit = prompt("What year is it?");
-
-if (wyit == 2020) {
-    alert("IT'S HAPPENING AGAIN")
-} else if (wyit < 2020) {
-    alert('You must be a time traveling wizard') 
-} else if (wyit > 2020) {
-    let covid = prompt("Is the pandemic over?");  
-    if (covid == yes) {
-        alert('Ok, it must be safe to come out now')
-    } else {
-        alert('What ever are we going to do now?')
-    }
-} else {
-    alert('they are good dogs brant')
+function promptuserforyear() {
+    // prompt
+    let wyit = prompt('What year is it?')
+    //change response
+    if (wyit == 2020) {
+    yr = "IT'S HAPPENING AGAIN";
+}     
+    else if (wyit < 2020) {
+    yr = 'You must be a time traveling wizard';
+}   
+    else if (wyit > 2020) {
+    yr = "Stay Masked and stay safe fren";  
+} 
+    else {
+    //call for start of function for valid response
+    alert('Not a valid response, please use numeric characters')
+    promptuserforyear();
 }
-
-
-
+    return alert(yr)
+    }
+    promptuserforyear()
+    
+// if (covid === yes) {
+//     alert('Ok, it must be safe to come out now')
+// } else {
+//     alert('Stay masked and stay safe')
+// }
 
 function change()
 {
     var coin = document.getElementById("flip");
     if (coin.value=="heads") coin.value = "tails";
     else coin.value = "heads";
+    console.log('Coin', coin);
 }
-console.log('coin', coin);
+
 
