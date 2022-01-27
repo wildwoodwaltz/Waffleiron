8ec3d014bf33f600fcbde66c593202e10d43cdf5
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-01-27 08:22:55 -0800

    fixed a lot of JS

--- js/active.js
+++ js/active.js
@@ -1,6 +1,31 @@
 
 alert("Before we begin, there are a few questions we would like to ask.");
 
-bing = prompt("What year is it?", );
-bang = prompt("")
+let wyit = prompt("What year is it?");
+
+if (wyit == 2020) {
+    alert("IT'S HAPPENING AGAIN")
+} else if (wyit < 2020) {
+    alert('You must be a time traveling wizard') 
+} else if (wyit > 2020) {
+    let covid = prompt("Is the pandemic over?");  
+    if (covid == yes) {
+        alert('Ok, it must be safe to come out now')
+    } else {
+        alert('What ever are we going to do now?')
+    }
+} else {
+    alert('they are good dogs brant')
+}
+
+
+
+
+function change()
+{
+    var coin = document.getElementById("flip");
+    if (coin.value=="heads") coin.value = "tails";
+    else coin.value = "heads";
+}
+console.log('coin', coin);
 
