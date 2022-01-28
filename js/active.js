9fe333a5aec823012378e38be4f0c378eea014a2
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-01-27 16:36:52 -0800

    Added loop to get image button

--- js/active.js
+++ js/active.js
@@ -2,34 +2,36 @@
 
 alert("Before we begin, there are a few questions we would like to ask.");
 
+//#########################################################
 function promptuserforyear() {
     // prompt
     let wyit = prompt('What year is it?')
-    //change response
-    if (wyit == 2020) {
+    //based on input change response
+    if (wyit === '') {
+        //call for start of function for valid response
+        alert('Not a valid response, please use numeric characters')
+        promptuserforyear();
+}
+    else if (wyit == 2020) {
     yr = "IT'S HAPPENING AGAIN";
-}     
+} 
     else if (wyit < 2020) {
     yr = 'You must be a time traveling wizard';
 }   
     else if (wyit > 2020) {
-    yr = "Stay Masked and stay safe fren";  
+    yr = "Stay masked and stay safe fren";  
 } 
     else {
-    //call for start of function for valid response
     alert('Not a valid response, please use numeric characters')
     promptuserforyear();
 }
     return alert(yr)
     }
-    promptuserforyear()
-    
-// if (covid === yes) {
-//     alert('Ok, it must be safe to come out now')
-// } else {
-//     alert('Stay masked and stay safe')
-// }
+//------------------------------------------------------------
 
+promptuserforyear();
+
+//###########################################################
 function change()
 {
     var coin = document.getElementById("flip");
@@ -37,5 +39,24 @@ function change()
     else coin.value = "heads";
     console.log('Coin', coin);
 }
+//-----------------------------------------------------------
 
+//###########################################################
+function imgprompt(promptimage){
+    var executed = false;
+    if (!promptimage) {
+        promptimage = 'how many pictures would you like?';
+    }
 
+    let imgct = prompt(promptimage)
+
+    if(imgct === ''){
+        let error = 'please enter a number between 1 and 3'
+        imgprompt(promptimage + error);
+    } else if(imgct <= 3) {
+        for(let i = 0; i < imgct; i++){
+             document.getElementById("imgs").innerHTML += '<img src="./vivandrupert.png">';
+        }
+    }    
+}
+//-----------------------------------------------------------
